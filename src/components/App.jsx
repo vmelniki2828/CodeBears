8b4285559883a4221bf55f0763b39d5630b0e36e
Vlad Melnik <vmelniki2828@gmail.com>
2024-12-01 14:31:05 +0200

    edit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import { Parallax } from "react-scroll-parallax";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div style={{display:"flex", gap:"50px"}}>
+      <Parallax speed={-10}>
+        <div
+          style={{ width: '400px', backgroundColor: 'green', height: '500px' }}
+        />
+      </Parallax>
+      <Parallax speed={10}>
+        <div
+          style={{ width: '400px', backgroundColor: 'green', height: '500px' }}
+        />
+      </Parallax>
+      
     </div>
   );
 };
