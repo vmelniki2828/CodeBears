9004b01c5019cd182f63faa4ccdf96c065188a55
Vlad Melnik <vmelniki2828@gmail.com>
2024-12-03 04:43:58 +0200

    екн

--- src/Pages/Melya/MelyaPage.jsx
+++ src/Pages/Melya/MelyaPage.jsx
@@ -1,14 +1,50 @@
-import { ParallaxBanner } from 'react-scroll-parallax';
-import backgroundImage from '../../components/back2.jpg';
+import { useGSAP } from '@gsap/react';
+import {
+  CardConatiner,
+  MainComponent,
+  MainTitle,
+  SubTitle,
+} from './MelyaPage.styled';
+import card from './card.png';
+import card2 from './card2.png';
+import card3 from './card3.png';
+import { useLayoutEffect, useRef } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
 
+gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(ScrollTrigger);
 const MelyaPage = () => {
+  const container = useRef();
+
+  useGSAP(
+    () => {
+      gsap.to('.card', { rotateY: 30, rotationX : 60, rotationZ: -10, x: 100});
+      gsap.to('.card2', { rotateY: 30, rotationX : 60, rotationZ: -10});
+      gsap.to('.card3', { rotateY: 30, rotationX : 60, rotationZ: -10, x: -100});
+    },
+    { scope: container }
+  ); 
+
+  useLayoutEffect(()=>{
+    gsap.from(".card", {
+      x:500,
+      scrollTrigger:{
+        trigger:".card",
+      }
+    })
+  },[])
+
   return (
-    <div>
-      <ParallaxBanner
-        layers={[{ image: backgroundImage, speed: -15 }]}
-        style={{ height: '100vh' }} // Пример с явной высотой
-      />
-    </div>
+    <MainComponent>
+      <MainTitle>3D Animating</MainTitle>
+      <SubTitle>Credit Card</SubTitle>
+      <CardConatiner ref={container} className="app">
+        <img className="card" src={card} />
+        <img className="card2" src={card2} />
+        <img className="card3"  src={card3} />
+      </CardConatiner>
+    </MainComponent>
   );
 };
 
---
+++ src/Pages/Melya/MelyaPage.styled.jsx
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+export const MainComponent = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: #121212;
+  padding-top: 186px;
+`;
+
+export const MainTitle = styled.h2`
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 900;
+  font-size: 92px;
+  line-height: 138px;
+  text-align: center;
+  color: #e5f86a;
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+  margin: 0;
+`;
+
+export const SubTitle = styled.p`
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 92px;
+  line-height: 138px;
+  text-align: center;
+  color: #ffffff;
+  flex: none;
+  order: 1;
+  flex-grow: 0;
+  margin: 0;
+  margin-bottom: 50px;
+`;
+
+export const CardConatiner = styled.div`
+    display: flex;
+    justify-content: center;
+`
