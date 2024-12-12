72c341e003532292b91979c90aaad1c432fa687a
Vlad Melnik <vmelniki2828@gmail.com>
2024-12-12 03:56:59 +0200

    side bar edit

--- src/components/SideBarMenu/SideBarMenu.jsx
+++ src/components/SideBarMenu/SideBarMenu.jsx
@@ -1,29 +1,81 @@
-import React, { useEffect } from "react";
-import gsap from "gsap"; // Импортируем gsap для анимации
-import { SideBarContainer } from "./SideBarMenu.styled"; // Ваш стиль для сайдбара
+import React, { useEffect } from 'react';
+import gsap from 'gsap'; // Импортируем gsap для анимации
+import {
+  SideBarContainer,
+  SideBarMenuList,
+  SideBarListLeft,
+  SideBarListRight,
+  SideBarListLeftItem,
+  SideBarListRightLeft,
+  SideBarListRightRight,
+  SideBarListRightLeftItems,
+  SideBarListRightRightItems,
+  FixedBottomBlock,
+  FixedBottomBlockText,
+} from './SideBarMenu.styled'; // Ваш стиль для сайдбара
 
 const SideBarMenu = ({ isOpen }) => {
-    useEffect(() => {
-        if (isOpen) {
-            gsap.to('.sideMenu', {
-                x: 0,
-                opacity: 1,
-                duration: 0.4,
-            });
-        } else {
-            gsap.to('.sideMenu', {
-                x: '-100vw',
-                opacity: 0,
-                duration: 0.4,
-            });
-        }
-    }, [isOpen]); // Перезапускаем анимацию, если `isOpen` изменяется
+  useEffect(() => {
+    if (isOpen) {
+      gsap.to('.sideMenu', {
+        x: 0,
+        opacity: 1,
+        duration: 0.4,
+      });
+    } else {
+      gsap.to('.sideMenu', {
+        x: '-100vw',
+        opacity: 0,
+        duration: 0.4,
+      });
+    }
+  }, [isOpen]); // Перезапускаем анимацию, если `isOpen` изменяется
 
-    return (
-        <SideBarContainer className="sideMenu">
-            Side Menu
-        </SideBarContainer>
-    );
+  return (
+    <>
+      <SideBarContainer className="sideMenu">
+        <SideBarMenuList>
+          <SideBarListLeft>
+            <SideBarListLeftItem>Головна</SideBarListLeftItem>
+            <SideBarListLeftItem>Про нас</SideBarListLeftItem>
+            <SideBarListLeftItem>Наші проєкти</SideBarListLeftItem>
+            <SideBarListLeftItem>Контактна інформація</SideBarListLeftItem>
+          </SideBarListLeft>
+          <SideBarListRight>
+            <SideBarListRightLeft>
+              <SideBarListRightLeftItems>Послуги</SideBarListRightLeftItems>
+              <SideBarListRightLeftItems>Брендинг</SideBarListRightLeftItems>
+              <SideBarListRightLeftItems>Промо-сайти</SideBarListRightLeftItems>
+              <SideBarListRightLeftItems>e-Commerce</SideBarListRightLeftItems>
+              <SideBarListRightLeftItems>
+                Мобільні додатки
+              </SideBarListRightLeftItems>
+              <SideBarListRightLeftItems>
+                Корпоративні сайти
+              </SideBarListRightLeftItems>
+              <SideBarListRightLeftItems>
+                Аудит сайтів
+              </SideBarListRightLeftItems>
+            </SideBarListRightLeft>
+            <SideBarListRightRight>
+              <SideBarListRightRightItems>Портфоліо</SideBarListRightRightItems>
+              <SideBarListRightRightItems>Брендинг</SideBarListRightRightItems>
+              <SideBarListRightRightItems>
+                UX/UI дизайн
+              </SideBarListRightRightItems>
+              <SideBarListRightRightItems>Веб сайти</SideBarListRightRightItems>
+              <SideBarListRightRightItems>
+                Мобільні додатки
+              </SideBarListRightRightItems>
+            </SideBarListRightRight>
+          </SideBarListRight>
+        </SideBarMenuList>
+        <FixedBottomBlock>
+          <FixedBottomBlockText>Давайте поговоримо про ваш проєкт</FixedBottomBlockText>
+        </FixedBottomBlock>
+      </SideBarContainer>
+    </>
+  );
 };
 
 export default SideBarMenu;
--- src/components/SideBarMenu/SideBarMenu.styled.jsx
+++ src/components/SideBarMenu/SideBarMenu.styled.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const SideBarContainer = styled.div`
   position: fixed;
@@ -6,10 +6,122 @@ export const SideBarContainer = styled.div`
   left: 0;
   width: 100vw;
   height: 100vh;
-  background-color: #333;
+  background-color: #0b1323;
   color: #fff;
-  padding: 20px;
-  cursor: pointer;
   opacity: 0;
   z-index: 11;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const SideBarMenuList = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 100px 150px 0 150px;
+`;
+
+export const SideBarListLeft = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
+`;
+
+export const SideBarListRight = styled.div`
+  display: flex;
+  width: 50%;
+`;
+
+export const SideBarListRightLeft = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  gap: 26px;
+`;
+
+export const SideBarListRightLeftItems = styled.p`
+  margin: 0;
+  font-family: 'Freigeist';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 25.845px;
+  line-height: 32px;
+  color: #b1c5b9;
+  transition: color 0.3s ease-in-out; // Добавляем плавный переход цвета
+
+  &:hover {
+    color: #e7fff0; // Цвет при наведении
+    cursor: pointer; // Изменяем курсор на указатель
+  }
+`;
+
+export const SideBarListRightRight = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  gap: 26px;
+`;
+
+export const SideBarListRightRightItems = styled.p`
+  margin: 0;
+  font-family: 'Freigeist';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 25.845px;
+  line-height: 32px;
+  color: #b1c5b9;
+  transition: color 0.3s ease-in-out; // Добавляем плавный переход цвета
+
+  &:hover {
+    color: #e7fff0; // Цвет при наведении
+    cursor: pointer; // Изменяем курсор на указатель
+  }
+`;
+
+export const SideBarListLeftItem = styled.p`
+  margin: 0;
+  font-family: 'Freigeist';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 25.845px;
+  line-height: 33px;
+  color: #e7fff0;
+  transition: color 0.3s ease-in-out; // Добавляем плавный переход цвета
+
+  &:hover {
+    color: #57ff9a; // Цвет при наведении
+    cursor: pointer; // Изменяем курсор на указатель
+  }
+`;
+
+export const FixedBottomBlock = styled.div`
+  width: 100vw;
+  height: 150px;
+  background: linear-gradient(to right, red 42vw, blue 42vw);
+  color: white;
+  font-size: 24px;
+  border: none;
+  cursor: pointer;
+  text-transform: uppercase;
+  font-weight: bold;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.2);
+
+  font-family: 'Freigeist';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 60px;
+  line-height: 76px;
+  text-transform: uppercase;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const FixedBottomBlockText = styled.p`
+  width: 100vw;
+  color: transparent; /* Make the text transparent */
+  text-align: center;
+  background-clip: text;
+  -webkit-background-clip: text; /* For older WebKit browsers */
+  background-image: linear-gradient(to right, blue 42vw, red 42vw);
 `;
