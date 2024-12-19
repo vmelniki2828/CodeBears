2030ff63f53bccedebe0dfbeec2cfbbcde7fd9b0
vmelniki2828 <vmelniki2828@gmail.com>
2024-12-19 18:37:01 +0200

    edits

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,5 +1,5 @@
 import { BurgerIcon } from "components/Welcome/Welcome.styled"
-import { BackArrow, ServicesContainer, ServicesMainContainer } from "./Services.styled"
+import { BackArrow, ListContainer, MainListContainer, MainListItem, ServicesContainer, ServicesLeftBottomContainer, ServicesLeftContainer, ServicesLeftTopContainer, ServicesLeftTopTitle, ServicesMainContainer, ServicesMainTitle, ServicesRightContainer, ServicesRightBottomContainer } from "./Services.styled"
 import SideBarMenu from "components/SideBarMenu/SideBarMenu"
 import { useEffect, useState } from "react";
 import gsap from "gsap";
@@ -26,7 +26,78 @@ const Services = () => {
             <SideBarMenu isOpen={sidebarSwitcher} />
 
             <ServicesMainContainer>
-                dsadasda
+                <ServicesLeftContainer>
+                    <ServicesLeftTopContainer>
+                        <ServicesLeftTopTitle>Розробка.</ServicesLeftTopTitle>
+                        <MainListContainer>
+                            <ListContainer>
+                                <MainListItem>PWA (Progressive Web Apps) </MainListItem>
+                                <MainListItem>API розробка </MainListItem>
+                                <MainListItem>Big Data та аналітика </MainListItem>
+                                <MainListItem>Автоматизація та оптимізація бізнес-процесів </MainListItem>
+                                <MainListItem>Розробка електронних журналов і систем обліку </MainListItem>
+                                <MainListItem>Платформи для онлайн-освіти</MainListItem>
+                            </ListContainer>
+                            <ListContainer>
+                                <MainListItem>Системи документообігу</MainListItem>
+                                <MainListItem>ERP и CRM системи</MainListItem>
+                                <MainListItem>Корпоративні сайти </MainListItem>
+                                <MainListItem>Інтернет-магазини </MainListItem>
+                                <MainListItem>Односторінкові сайти (landing page)  </MainListItem>
+                                <MainListItem>Мобільні додатки  </MainListItem>
+                                <MainListItem>ШІ </MainListItem>
+                            </ListContainer>
+                        </MainListContainer>
+
+                    </ServicesLeftTopContainer>
+                    <ServicesLeftBottomContainer>
+                        <ServicesLeftTopTitle>МАРКЕТИНГ.</ServicesLeftTopTitle>
+                        <MainListContainer>
+                            <ListContainer>
+                                <MainListItem>Банерна реклама на сайтах</MainListItem>
+                                <MainListItem>SMM</MainListItem>
+                                <MainListItem>Контекстна реклама</MainListItem>
+                                <MainListItem>Seo оптимізація</MainListItem>
+                                <MainListItem>E-mail розсилки</MainListItem>
+                                <MainListItem>Аналіз конкурентів</MainListItem>
+                            </ListContainer>
+                            <ListContainer>
+                                <MainListItem>Веб аналітика</MainListItem>
+                                <MainListItem>Контент план</MainListItem>
+                                <MainListItem>Таргетована реклама</MainListItem>
+                                <MainListItem>Маркетингова стратегія</MainListItem>
+                                <MainListItem>Маркетинговий аудит</MainListItem>
+                                <MainListItem>Аналіз конкурентів</MainListItem>
+                            </ListContainer>
+                        </MainListContainer>
+                    </ServicesLeftBottomContainer>
+                </ServicesLeftContainer>
+                <ServicesRightContainer>
+                    <ServicesMainTitle>ПОСЛУГИ</ServicesMainTitle>
+                    <ServicesRightBottomContainer>
+                        <ServicesLeftTopTitle>ДИЗАЙН.</ServicesLeftTopTitle>
+                        <MainListContainer>
+                            <ListContainer>
+                                <MainListItem>Рекламні банери</MainListItem>
+                                <MainListItem>Моушн дизайн</MainListItem>
+                                <MainListItem>Бренд Бук</MainListItem>
+                                <MainListItem>Веб дизайн</MainListItem>
+                                <MainListItem>UX/UI дизайн </MainListItem>
+                                <MainListItem>Іллюстрації та іконографіка</MainListItem>
+                                <MainListItem>3D моделювання і визуалізація</MainListItem>
+                                <MainListItem>Інфографіка</MainListItem>
+                            </ListContainer>
+                            <ListContainer>
+                                <MainListItem>Пакетний дизайн </MainListItem>
+                                <MainListItem>Типографіка і леттерінг</MainListItem>
+                                <MainListItem>Дизайн для соціальних мереж</MainListItem>
+                                <MainListItem>Анимація та відео дизайн </MainListItem>
+                                <MainListItem>Розробка фірмового стилю</MainListItem>
+                                <MainListItem>Ретуш та обробка фотографій</MainListItem>
+                            </ListContainer>
+                        </MainListContainer>
+                    </ServicesRightBottomContainer>
+                </ServicesRightContainer>
             </ServicesMainContainer>
 
         </ServicesContainer>)
--- src/components/Services/Services.styled.js
+++ src/components/Services/Services.styled.js
@@ -5,7 +5,6 @@ import { FiArrowUpLeft } from "react-icons/fi";
 export const ServicesContainer = styled.div`
 background: radial-gradient(145.11% 101.78% at 50% 50%, #06050A 0%, #0F1924 100%) /* warning: gradient uses a rotation that is not supported by CSS and may not behave as expected */;
 width:100%;
-height:500px;
 `
 
 export const BackArrow = styled(FiArrowUpLeft)`
@@ -26,4 +25,89 @@ export const BackArrow = styled(FiArrowUpLeft)`
 export const ServicesMainContainer = styled.div`
   padding: 120px 100px 150px 100px;
   color: #fff;
+  display: flex;
+  justify-content: space-around;
+  gap:150px;
+`
+
+export const ServicesLeftContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap:70px;
+`
+
+export const ServicesLeftTopContainer = styled.div`
+  border-top: 1px solid white; /* белая верхняя граница */
+`
+
+export const ServicesLeftTopTitle = styled.h2`
+font-family: 'Gunterz', sans-serif;
+font-style: normal;
+font-weight: 500;
+font-size: 22px;
+line-height: 86.5%;
+/* identical to box height, or 19px */
+letter-spacing: -0.02em;
+color: #FFFFFF;
+margin: 15px 0 30px 0;
+  text-transform: uppercase;
+
+`
+
+export const MainListContainer = styled.div`
+  display:flex;
+  gap:30px;
+`
+
+export const ListContainer = styled.div`
+  display:flex;
+  flex-direction:column;
+  width:261px;
+  gap:25px;
+`
+
+export const MainListItem = styled.p`
+  font-family: 'Freigeist', sans-serif;
+font-style: normal;
+font-weight: 400;
+font-size: 22px;
+line-height: 104%;
+letter-spacing: -0.02em;
+cursor: pointer;
+color: #FFFFFF;
+
+text-shadow: 0px 4px 16px rgba(0, 0, 0, 0.68);
+margin: 0;
+`
+
+export const ServicesLeftBottomContainer = styled.div`
+  border-top: 1px solid white; /* белая верхняя граница */
+`
+
+
+export const ServicesRightContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const ServicesMainTitle = styled.h2`
+  font-family: 'Freigeist', sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 113px;
+  line-height: 141px;
+  display: flex;
+  align-items: center;
+  text-align: right;
+  margin:0 0 100px 0;
+  background: linear-gradient(123.73deg, #FFFFFF -44.77%, #45AEB1 302.21%);
+  -webkit-background-clip: text;      
+  -webkit- text-fill-color: transparent;            
+  background-clip:  text;
+  text-fill-color: transparent;
+  height:74px;
+`
+
+export const ServicesRightBottomContainer = styled.div`
+  border-top: 1px solid white;
 `
