abe76da4d6a782c1e9c8124b8eebb6cceba13bc6
Ponedilok Danyyl <danya253545@gmail.com>
2024-12-29 20:39:41 +0200

    Helmet

--- src/Pages/PortfolioPage/PortfolioPage.jsx
+++ src/Pages/PortfolioPage/PortfolioPage.jsx
@@ -1,9 +1,15 @@
 import Footer from 'components/Footer/Footer';
 import Portfolio from 'components/Portfolio/Portfolio';
+import { Helmet } from 'react-helmet';
 
 const PortfolioPage = () => {
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>My PortfolioPage</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
       <Portfolio />
       <Footer />
     </>
--- src/Pages/ServicesPage/ServicesPage.jsx
+++ src/Pages/ServicesPage/ServicesPage.jsx
@@ -1,10 +1,16 @@
 import Footer from 'components/Footer/Footer';
 import Services from 'components/Services/Services';
+import { Helmet } from 'react-helmet';
 import { Outlet } from 'react-router-dom';
 
 const ServicesPage = () => {
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>My ServicesPage</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
       <Services />
       <Footer />
     </>
