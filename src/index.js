d0524d5216348ef70fe5c3f289865270687331a6
vmelniki2828 <vmelniki2828@gmail.com>
2024-12-29 08:02:47 +0200

    edit

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ParallaxProvider>
-      <BrowserRouter>
+      <BrowserRouter basename='CodeBears'>
         <App />
       </BrowserRouter>
     </ParallaxProvider>
