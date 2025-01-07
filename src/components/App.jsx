10ed2ddf2c5c810b9f39af0d6e7ef39175c0e0b2
Kyrylo Dyachenko <104459315+kirill25460@users.noreply.github.com>
2025-01-07 22:52:39 +0200

    change loading

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,8 @@
+import { useState, useEffect } from 'react';
 import { Suspense, lazy } from 'react';
 import { Link, Route, Router, Routes } from 'react-router-dom';
-import { Load } from './App.styled';
-import { MutatingDots } from 'react-loader-spinner';
+import { Load,CustomLoader } from './App.styled';
+
 
 import ScrollToTop from 'addition/ScrollToTop/ScrollToTop';
 
@@ -11,21 +12,13 @@ const PWAPage = lazy(() => import('Pages/ServicesPage/PWAPage/PWAPage'));
 const ServicesPage = lazy(() => import('Pages/ServicesPage/ServicesPage'));
 
 export const App = () => {
+
   return (
     <div>
       <Suspense
         fallback={
           <Load>
-            <MutatingDots
-              height="100"
-              width="100"
-              color="#AFFFB7"
-              secondaryColor="#AFFFB7"
-              radius="12.5"
-              ariaLabel="mutating-dots-loading"
-              wrapperStyle={{}}
-              wrapperClass=""
-            />
+          <CustomLoader />
           </Load>
         }
       >
--- src/components/App.styled.js
+++ src/components/App.styled.js
@@ -1,7 +1,18 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+import moonImage from '../image/moonWithBear.png';
+const rotate = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
 
 export const Load = styled.div`
   z-index: -999;
+  height: 100%;
+  width: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -10,4 +21,16 @@ export const Load = styled.div`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+  background-color: #000;
 `;
+
+const RotatingMoon = styled.img`
+  width: 550px; /* Размер изображения */
+  height: 550px;
+  animation: ${rotate} 3s linear infinite; /* Время анимации и бесконечный цикл */
+`;
+
+export const CustomLoader = () => {
+  return <RotatingMoon src={moonImage} alt="Loading..." />;
+};
+
